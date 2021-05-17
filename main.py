d608942dfab668b3dceeac99af3402e48fa80ea7
Sander Huijsen <sander.huijsen@gmail.com>
2021-05-17 18:46:11 +0800

    Added comments. The big number now starts as '---'.

--- main.py
+++ main.py
@@ -8,6 +8,20 @@ logger.setLevel(logging.INFO)
 
 
 class CountdownGui(EasyFrame):
+    """The CountdownGui class creates a visual representation of the
+    Numbers game that is part of Countdown / Letters and Numbers. It
+    shows the number to be reached (from 101-999) as a really big
+    number (as in size), and the small numbers with which to make it.
+
+    The small numbers available are 1-10 (each 2x), and the big numbers
+    are 25, 50, 75, and 100 (each 1x).
+
+    In the game, the player can choose to select 0 - 4 "big numbers",
+    while the rest is automatically made up of small numbers. In total,
+    six numbers are selected.
+
+    By pressing the yellow button, a new number can be selected.
+    """
     SMALL_NUMBERS = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8, 9, 9, 10, 10]
     BIG_NUMBERS = [25, 50, 75, 100]
 
@@ -26,7 +40,7 @@ class CountdownGui(EasyFrame):
             self.canvas.drawText('0', 75 + 150 * x, 100, font=("Courier", 150, 'bold'),
                                  tag=f'bignum-{x}', fill='lightgreen')
 
-        self.create_random_bignum(guess=101)
+        self.__show_bignum('---')
         self.addCanvas(canvas=RedButton(self, on_click=self.create_random_bignum),
                        row=1, column=6)
 
@@ -45,33 +59,36 @@ class CountdownGui(EasyFrame):
         """This method draws a Big Number on the canvas. It splits
         the number into three digits. Each is shown separately on
         the canvas.
+
+        Technically it takes a string of length >= 3. This is not
+        checked, so the caller must make sure this is the case.
         """
         fill = 'lightgreen'
         font = ("Courier", 150, 'bold')
-        num = str(num)
         for x in range(3):
             self.canvas.delete(f'bignum-{x}')
             self.canvas.drawText(num[x], 75 + 150 * x, 100, font=font,
                                  tag=f'bignum-{x}', fill=fill)
 
-    def create_random_bignum(self, guess=None):
+    def create_random_bignum(self):
         """Create a random number and show it as a Big Number.
         If a guess is provided, it is simply shown. If no guess
         is provided, the program will randomly select one,
-        while showing the selection process.
+        while showing the selection process. It shows numbers
+        really quickly while slowing down over 2-3 seconds.
         """
-        if guess:
-            self.__show_bignum(guess)
-        else:
-            timeout = 0.015
-            for x in range(25):
-                guess = random.randint(101, 999)
-                self.__show_bignum(guess)
-                if x % 5 == 1:
-                    timeout += 0.025
+        timeout = 0.0
+        for x in range(25):
+            time.sleep(timeout)
+            if x % 5 == 0:
+                timeout += 0.025
 
-                time.sleep(timeout)
-                self.update()
+            # Get a new random number
+            guess = random.randint(101, 999)
+            self.__show_bignum(str(guess))
+
+            # Make sure the display updates
+            self.update()
 
 
 class RedButton(EasyCanvas):
